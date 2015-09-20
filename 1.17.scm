290e7362f7724c7c5f427c55228825efc71ac56b
retamia <liumengte@howzhi.com>
2015-09-20 17:46:20 +0800

    迭代计算过程

--- 1.17.scm
+++ 1.17.scm
@@ -1,14 +1,33 @@
 (define (double n)
-  (+ n n))
+  (* 2 n))
 
 (define (halve n)
   (/ n 2))
 
-(define (* a b)
+
+;;递归计算过程
+(define (mul-rec a b)
   (if (= b 0)
       0
       (if (even? b)
-          (double (* a (halve b)))
-          (+ a (* a (- b 1)) ))))
+          (double (mul-rec a (halve b)))
+          (+ a (mul-rec a (- b 1)) ))))
 
-(* 2 2)
+;;迭代计算过程
+(define (mul-iter a b)
+  (iter a b 0))
+
+(define (iter a b n)
+  (if (= b 0)
+      n
+      (if (even? b)
+          (iter (double a) (halve b) n)
+          (iter a (- b 1) (+ a n)))))
+
+(mul-rec 2 100)
+
+
+
+   
+
+
