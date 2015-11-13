33e5df2ad4fef0484c368e7b2b86c8f04aef2c26
retamia <liumengte@howzhi.com>
2015-11-13 13:24:43 +0800

    练习1.36

---
+++ 1.36.rkt
@@ -0,0 +1,35 @@
+#lang planet neil/sicp
+
+(define (average x y)
+  (/ (+ x y)
+     2
+     ))
+
+(define tolerance 0.00001)
+
+
+(define (fixed-point f first-guess)
+  (define (display-info guess step)
+    (display "Step: ")
+    (display step)
+    (display "====")
+    (display "Guess: ")
+    (display guess)
+    (newline))
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess step)
+    (display-info guess step)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          (begin
+            (display-info next (+ 1 step))
+            next)
+          (try next (+ 1 step)))))
+  (try first-guess 1))
+
+;;不用平均阻尼
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
+
+;;平均阻尼
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 2.0)
