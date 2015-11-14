1be87920732853a22e946b2b54fcf58590f8a728
retamia <liumengte@howzhi.com>
2015-11-14 20:32:59 +0800

    练习1.42

--- 1.42.rkt
+++ 1.42.rkt
@@ -11,3 +11,11 @@
     (f(g x))))
 
 ((compose square inc) 6)
+
+(define (repeated f times)
+  (if (= times 1)
+      f
+      (compose f
+               (repeated f (- times 1)))))
+
+((repeated square 2) 5)
