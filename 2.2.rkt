c8eb182bc93036a5681976cd3e407ddb7b085831
retamia <liumengte@howzhi.com>
2015-11-18 12:36:03 +0800

    练习2.2

---
+++ 2.2.rkt
@@ -0,0 +1,44 @@
+#lang planet neil/sicp
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (midpoint-segment segment)
+  (make-point (/ (+ (x-point (start-segment segment))
+                    (x-point (end-segment segment)))
+                 2)
+              (/ (+ (y-point (start-segment segment))
+                    (y-point (end-segment segment)))
+                 2)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define start-point (make-point 0 0))
+
+(define end-point (make-point 0 2))
+
+(define segment (make-segment start-point end-point))
+
+(print-point (midpoint-segment segment))
+
